424a0594b56e8fc0e9be15413e8a4d2ccfee01ae
xavier_native <1832941391@qq.com>
2025-09-27 01:13:34 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div>这是文件首页</div>
+  <button @click="goList" type="primary">跳转list页面</button>
+  <button @click="$router.push('/node')" type="success">跳转home页面</button>
+  <p>{{ userStore.token }}</p>
+  <el-button @click="userStore.setToken('login')">登录</el-button>
+  <el-button @click="userStore.removeToken()">退出</el-button>
 </template>
 
-<script setup lang="ts">
-const obj = {
-  name: '张三',
-  age: 18
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push({
+    path: '/list'
+  })
+  console.log(route.query)
+  console.log(router)
 }
-console.log(obj);
+
+const userStore = useUserStore()
+
+
+
 </script>
 
 <style scoped></style>
