1455920442fd78e617975077c957f344fb1f4497
xavier_native <1832941391@qq.com>
2025-11-02 23:10:09 +0800

    24 25 文章列表 分页渲染、loading效果、处理搜索重置

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -15,12 +15,12 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">搜索</el-button>
-        <el-button>重置</el-button>
+        <el-button @click="onSearch" type="primary">搜索</el-button>
+        <el-button @click="onReset">重置</el-button>
       </el-form-item>
     </el-form>
     <!-- 表格区域 -->
-    <el-table :data="articleList">
+    <el-table :data="articleList" :loading="loading">
       <el-table-column label="文章标题" prop="title">
         <template #default="scope">
           <el-link type="primary" :underline="false">{{ scope.row.title }}</el-link>
@@ -40,6 +40,18 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页区域 -->
+    <el-pagination
+      v-model:current-page="params.pagenum"
+      v-model:page-size="params.pagesize"
+      :page-sizes="[2, 3, 5, 10]"
+      :background="true"
+      layout="jumper, total, sizes, prev, pager, next"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      style="margin-top: 20px; justify-content: flex-end;"
+    />
   </PageContainer>
 </template>
 
@@ -50,12 +62,11 @@ import ChannelSelect from '@/views/article/components/ChannelSelect.vue'
 import { articleGetListService } from '@/api/article'
 import { formatTime } from '@/utils/format'
 
-
-
 const articleList = ref([])  // 文章列表
 const total = ref(0)  // 文章总数
+const loading = ref(false)  // 分页加载状态
 
-// 定义请求参数对象
+// 请求参数模型
 const params = ref({
   pagenum: 1,
   pagesize: 10,
@@ -63,25 +74,58 @@ const params = ref({
   state: ''
 })
 
-// 定义获取文章列表方法
+// 请求获取文章列表方法
 const getArticleList = async () => {
+  loading.value = true
+
   const res = await articleGetListService(params.value)
   console.log(res)
   if (res.data.code !== 0) return ElMessage.error('获取文章列表失败')
   articleList.value = res.data.data
   total.value = res.data.total
-}
-getArticleList()
 
-// 定义编辑文章方法
+  loading.value = false
+}
+
+// 搜索按钮点击事件
+const onSearch = () => {
+  params.value.pagenum = 1
+  getArticleList()
+}
+// 重置按钮点击事件
+const onReset = () => {
+  params.value = {
+    pagenum: 1,
+    pagesize: 5,
+    cate_id: '',
+    state: ''
+  }
+  getArticleList()
+}
+// 按键编辑文章方法
 const onEditArticle = (row) => {
   console.log('编辑文章', row)
 }
-// 定义删除文章方法
+// 按键删除文章方法
 const onDeleteArticle = (row) => {
   console.log('删除文章', row)
 }
 
+// 处理分页展示数据条数变化
+const handleSizeChange = (size) => {
+  // 重置当前页码为第一页
+  params.value.pagenum = 1
+  params.value.pagesize = size
+  getArticleList()
+}
+// 处理分页当前页码变化
+const handleCurrentChange = (page) => {
+  params.value.pagenum = page
+  getArticleList()
+}
+
+// 页面初始化加载
+getArticleList()
 </script>
 
 <style scoped>
