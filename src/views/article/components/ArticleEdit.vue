77ddae6c8a87e1e6df6854e10c4703a2b7ac842a
xavier_native <1832941391@qq.com>
2025-11-15 19:43:57 +0800

    28 上传文件

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -16,7 +16,18 @@
           width="100%"
         ></channel-select>
       </el-form-item>
-      <el-form-item label="文章封面" prop="cover_img"> 文件上传 </el-form-item>
+      <el-form-item label="文章封面" prop="cover_img"> 
+        <!-- 关闭 element-plus 的自动上传功能，不需要 action 等参数 -->
+        <el-upload
+          class="avatar-uploader"
+          :auto-upload="false"
+          :show-file-list="false"
+          :on-change="onUploadFile"
+        >
+          <img v-if="imgUrl" :src="imgUrl" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
+      </el-form-item>
       <el-form-item label="文章内容" prop="content">
         <div class="editor">富文本编辑器</div>
       </el-form-item>
@@ -31,6 +42,7 @@
 <script setup>
 import { ref } from 'vue'
 import ChannelSelect from './ChannelSelect.vue'
+import { Plus } from '@element-plus/icons-vue'
 
 const visibleDrawer = ref(false)  // 抽屉组件是否可见
 
@@ -46,7 +58,6 @@ const defaultForm = {
 // 表单数据模型
 const formModel = ref({ ...defaultForm })  
 
-
 const open = (row) => {
   visibleDrawer.value = true
   // 如果是编辑操作，将传入的文章数据合并到表单数据模型中
@@ -58,10 +69,45 @@ const open = (row) => {
   console.log(row)
 }
 
+// 图形上传
+const imgUrl = ref('')
+const onUploadFile = (uploadFile) => {
+  imgUrl.value = URL.createObjectURL(uploadFile.raw)
+  formModel.value.cover_img = uploadFile.raw
+}
+
+
 defineExpose({
   open
 })
 </script>
 
 <style lang="scss" scoped>
+.avatar-uploader {
+  :deep() {
+    .avatar {
+      width: 178px;
+      height: 178px;
+      display: block;
+    }
+    .el-upload {
+      border: 1px dashed var(--el-border-color);
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+      transition: var(--el-transition-duration-fast);
+    }
+    .el-upload:hover {
+      border-color: var(--el-color-primary);
+    }
+    .el-icon.avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 178px;
+      height: 178px;
+      text-align: center;
+    }
+  }
+}
 </style>
